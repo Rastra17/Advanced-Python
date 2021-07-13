a3b3b6a25e303c7d46b01db287a6da6efb33cada
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2021-07-13 18:53:32 +0545

    My first programs in AI with python

---
+++ IDE/testTensor.py
@@ -0,0 +1,127 @@
+#Creating a program with TensorFlow
+
+#Importing TensorFlow
+import tensorflow as tf
+
+print(tf.__version__)
+
+#Creating tensors with tf.constant()
+scalar=tf.constant(10)
+
+#Printing the essence of scalar variable
+print(scalar)
+
+#Check the dimensions of a scalar tensor (ndim stands for number of dimensions)
+print("The dimensions of a scalar tensor is:",scalar.ndim)
+
+#Creating a vector
+vector=tf.constant([10,20])
+
+#Printing the essence of vector variable
+print(vector)
+
+#Check the dimensions of a vector tensor (ndim stands for number of dimensions)
+print("The dimensions of a vector tensor is:",vector.ndim)
+
+#Creating a matrix which is a data structure having more than 1 element
+matrix=tf.constant([
+[10,20],
+[20,10]
+])
+
+#Printing the essence of a matrix variable
+print(matrix)
+
+#Check the dimensions of a matrix tensor (ndim stands for number of dimensions)
+print("The dimensions of a matrix tensor is:",matrix.ndim)
+
+#Creating another matrix
+anotherMatrix=tf.constant([
+[40.,50.],
+[60.,70.],
+[80.,90.]
+],dtype=tf.float16) #Specifying the datatype with dtype parameter
+
+#Printing the essence of a specific matrix variable
+print(anotherMatrix)
+
+#Check the dimensions of a matrix tensor (ndim stands for number of dimensions)
+print("The dimensions of a specific matrix tensor is:",anotherMatrix.ndim)
+
+#Creating an original tensor
+tensor=tf.constant([
+[[1,2,3],
+[4,5,6]],
+
+[[7,8,9],
+[10,11,12]],
+
+[[13,14,15],
+[16,17,18]]
+])
+
+#Printing the essence of an original tensor
+print(tensor)
+
+#Check the dimensions of a tensor (ndim stands for number of dimensions)
+print("The dimensions of an original tensor is:",tensor.ndim)
+
+#What we have created so far
+#1. Scalar: Single number
+#2. Vector: A number with direction
+#3. Matrix: A two dimensional array of numbers
+#4. Tensor: An n-dimensional array of numbers
+
+#Creating a tensor variable
+changeableTensor=tf.Variable([10,20])
+
+#Creating a tensor constant
+unchangeableTensor=tf.constant([70,80])
+
+#Printing the essence values of the tensor variables
+print(changeableTensor)
+print(unchangeableTensor)
+
+#Let's try changing the values within the changeableTensor variable
+changeableTensor[0].assign(30)
+print(changeableTensor)
+
+#Rarely in practice will you need to decide whether to use tf.constant
+#or tf.variable to create tensors, as TensorFlow does this for you.
+#However, when in doubt, use tf.constant and change it later if needed.
+
+#Creating random tensors which has some arbitiary size and random numbers.
+random1=tf.random.Generator.from_seed(40) #Set seed for same results
+random1=random1.normal(shape=(3,2))
+
+random2=tf.random.Generator.from_seed(40) #Set seed for same results
+random2=random2.normal(shape=(3,2))
+
+#Checking if the two tensors above is equal or not
+print(random1==random2)
+
+#Printing the essence values of random1 and random2 variables
+print(random1)
+print(random2)
+
+#Shuffle the elements within a tensor to help make the machine learn efficiently
+notShuffled=tf.constant([
+[10,7],
+[3,4],
+[2,5]
+])
+
+#Check the dimensions of a matrix tensor (ndim stands for number of dimensions)
+print("The dimensions of an unshuffled matrix tensor is:",notShuffled.ndim)
+
+#Printing the essence values of notShuffled variable
+print(notShuffled)
+
+#Shuffling our notShuffled tensor
+shuffled=tf.random.shuffle(notShuffled,seed=40)
+
+#Check the dimensions of a matrix tensor (ndim stands for number of dimensions)
+print("The dimensions of a shuffled matrix tensor is:",shuffled.ndim)
+
+#Printing the essence values of shuffled varaible
+print(shuffled)
---
+++ IDE/ticTacToe.py
@@ -0,0 +1,129 @@
+#A tictactoe game with AI
+class tictactoe:
+    def __init__(self):
+        self.board=[' ' for x in range(10)]
+
+    def insertLetter(self,letter,pos):
+        self.board[pos]=letter
+
+    def spaceIsFree(self,pos):
+        return self.board[pos]==' '
+
+    def printBoard(self,board):
+        print("   |   |")
+        print(' '+self.board[1]+" | "+self.board[2]+" | "+self.board[3])
+        print("   |   |")
+        print("----------")
+        print("   |   |")
+        print(' '+self.board[4]+" | "+self.board[5]+" | "+self.board[6])
+        print("   |   |")
+        print("----------")
+        print("   |   |")
+        print(' '+self.board[7]+" | "+self.board[8]+" | "+self.board[9])
+        print("   |   |")
+
+    def isWinner(self,bo,le):
+        return ((bo[1]==le and bo[2]==le and bo[3]==le) or
+        (bo[4]==le and bo[5]==le and bo[6]==le) or
+        (bo[7]==le and bo[8]==le and bo[9]==le) or
+        (bo[1]==le and bo[4]==le and bo[7]==le) or
+        (bo[2]==le and bo[5]==le and bo[8]==le) or
+        (bo[3]==le and bo[6]==le and bo[9]==le) or
+        (bo[1]==le and bo[5]==le and bo[9]==le) or
+        (bo[3]==le and bo[5]==le and bo[7]==le))
+
+    def playerMove(self):
+        run=True
+
+        while (run):
+            move=int(input
+            ("Please select a position to place \'X\' from 1 to 9: "))
+
+            if(move>0 and move<10):
+                if(self.spaceIsFree(move)):
+                    run=False
+                    self.insertLetter('X',move)
+                else:
+                    print("This position is taken!")
+            else:
+                print("Choose a valid option!")
+
+    def compMove(self):
+        possibleMoves=[x for x, letter in enumerate(self.board)
+        if letter==' ' and x!=0]
+        move=0
+
+        for let in ['O','X']:
+            for i in possibleMoves:
+                boardCopy=self.board[:]
+                boardCopy[i]=let
+
+                if self.isWinner(boardCopy,let):
+                    move=i
+                    return move
+
+        cornersOpen=[]
+        for i in possibleMoves:
+            if i in[1,3,7,9]:
+                cornersOpen.append(i)
+
+        if len(cornersOpen) > 0:
+            move=self.selectRandom(cornersOpen)
+            return move
+
+        if 5 in possibleMoves:
+            move=5
+            return move
+
+        edgesOpen=[]
+        for i in possibleMoves:
+            if i in[2,4,6,8]:
+                edgesOpen.append(i)
+
+        if len(edgesOpen) > 0:
+            move=self.selectRandom(edgesOpen)
+            return move
+
+        return move
+
+    def selectRandom(self,li):
+        import random
+        ln=len(li)
+        r=random.randrange(0,ln)
+        return li[r]
+
+    def isBoardFull(self,board):
+        if self.board.count(' ')>1:
+            return False
+        else:
+            return True
+
+    def main(self):
+        print("Welcome to Tic Tac Toe")
+        self.printBoard(self.board)
+
+        while not(self.isBoardFull(self.board)):
+            if not(self.isWinner(self.board,'O')):
+                self.playerMove()
+                self.printBoard(self.board)
+            else:
+                print("O\'s won the game!")
+                break
+
+            if not(self.isWinner(self.board,'X')):
+                move=self.compMove()
+
+                if(move==0 or self.isBoardFull(self.board)):
+                    print("Tie Game!")
+                else:
+                    self.insertLetter('O',move)
+                    print("Computer placed an \'O\' in position",move)
+                    self.printBoard(self.board)
+
+            else:
+                print("X\'s won the game!")
+                break
+
+if __name__=='__main__':
+    game=tictactoe()
+    game.main()
